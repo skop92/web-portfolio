0dda9e95e83eb0272d8a152d0553776ee37af6f1
skop92 <samu.j.koponen@gmail.com>
2023-05-05 08:30:30 +0300

    Use helmet with own csp

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const helmet = require('helmet');
 
 app.use(express.static(path.join(__dirname, '/src')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
+app.use(helmet());
+
+const scriptSrcUrls = [
+    "'unsafe-inline'",
+];
+const styleSrcUrls = [
+    "https://fonts.googleapis.com",
+    "'unsafe-inline'",
+];
+const connectSrcUrls = [];
+const fontSrcUrls = [
+	"https://fonts.gstatic.com",
+];
+const imgSrcUrls = [
+	'blob:',
+    'data:',
+];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", ...styleSrcUrls],
+            workerSrc: ["'self'", 'blob:'],
+            childSrc: ['blob:'],
+            objectSrc: [],
+            imgSrc: ["'self'", ...imgSrcUrls],
+            fontSrc: ["'self'", ...fontSrcUrls],
+			//upgradeInsecureRequests: null // To allow http in local network
+        }
+    })
+);
 
 /* GET */
 
@@ -23,6 +57,7 @@ app.get('/portfolio-articles/:article', (req, res) => {
 	res.render(article, { active: "article" } );
 });
 
-app.listen(3000, () => {
-	console.log("LISTENING ON PORT 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`LISTENING ON PORT ${port}`);
 });
